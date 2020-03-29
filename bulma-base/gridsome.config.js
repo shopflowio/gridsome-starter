b3e31da0ede77fbb19ffebd4bab943c11cfcf383
Torey Heinz <torey@teagles.io>
2020-03-29 17:21:12 -0400

    Adds markdown content pages

--- bulma-base/gridsome.config.js
+++ bulma-base/gridsome.config.js
@@ -1,10 +1,18 @@
-// This is where project configuration and plugin options are located. 
+// This is where project configuration and plugin options are located.
 // Learn more: https://gridsome.org/docs/config
 
 // Changes here require a server restart.
 // To restart press CTRL + C in terminal and run `gridsome develop`
 
 module.exports = {
-  siteName: 'Gridsome',
-  plugins: []
+  siteName: "Gridsome",
+  plugins: [
+    {
+      use: "@gridsome/vue-remark",
+      options: {
+        typeName: "ContentPage",
+        baseDir: "./content/pages"
+      }
+    }
+  ]
 }
